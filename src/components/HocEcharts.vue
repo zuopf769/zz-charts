229346d98cc52e22eb55498914175f9c4979ed96
左鹏飞 <zuopengfei02@meituan.com>
2020-08-13 01:03:29 +0800

    feat(build): build

---
+++ src/components/HocEcharts.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="uix-charts-parent" :style="parentStyle">
+    <base-echarts
+      v-if="isHasData"
+      :init-options="initOptions"
+      :options="options"
+      :auto-resize="true"
+      :theme="theme"
+      :chart-height="height"
+      :ec.sync="ec"
+      v-on="$listeners"
+    />
+    <slot v-if="!isHasData && !loading">
+      <empty-data :empty-text="emptyText" />
+    </slot>
+    <loading-chart v-if="loading" />
+  </div>
+</template>
+
+<script>
+import BaseEcharts from './BaseEcharts.vue'
+import EmptyData from './EmptyData.vue'
+import LoadingChart from './LoadingChart.vue'
+import Core from '@/core'
+import { options } from '@/constants'
+export default {
+  components: {
+    BaseEcharts,
+    EmptyData,
+    LoadingChart
+  },
+  mixins: [Core],
+  data() {
+    return {
+      options,
+      initOptions: {
+        renderer: 'canvas'
+      }
+    }
+  }
+}
+</script>
