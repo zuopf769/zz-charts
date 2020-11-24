b141b28c4ef7162f573a973c2469abd780da8d88
yanghao15 <yanghao15@meituan.com>
2020-11-24 17:37:37 +0800

    refactor(line): 修改line配置

---
+++ src/packages/bar/doc/individualLabel.vue
@@ -0,0 +1,56 @@
+<template>
+  <uix-bar-chart :data="chartData" :height="340" :settings="chartSettings" />
+</template>
+
+<script>
+export default {
+  created() {
+    this.chartData = {
+      dimensions: {
+        name: 'Week',
+        data: ['Mon.', 'Tue.', 'Wed.', 'Thu.']
+      },
+      measures: [
+        {
+          name: '销售额',
+          data: [1393, 3530, 2923, 1723]
+        },
+        {
+          name: '访问用户',
+          data: [1393, 15300, 2923, 1723]
+        },
+        {
+          name: '下单用户',
+          data: [1093, 3230, 2623, 1423]
+        },
+        {
+          name: '下单率',
+          data: [0.32, 0.26, 0.76, 0.49]
+        }
+      ]
+    }
+    this.chartSettings = {
+      showLine: ['下单率'],
+      yAxisName: ['金额/万', '数值', '比率'],
+      yAxisLabelType: ['normal', 'normal', 'percentage'],
+      secondMeaAxis: ['销售额'],
+      thirdMeaAxis: ['下单率'],
+      label: [
+        {
+          name: '访问用户',
+          config: {
+            show: true
+          }
+        },
+        {
+          name: '下单率',
+          config: {
+            show: true,
+            fontWeight: 'bold'
+          }
+        }
+      ]
+    }
+  }
+}
+</script>
--- src/packages/bar/index.md
+++ src/packages/bar/index.md
@@ -5,6 +5,11 @@
 :::include(src="./doc/base.vue")
 :::
 
+## 基础条形图
+
+:::include(src="./doc/simpleBar.vue")
+:::
+
 ## 分组柱状图
 
 > 多个度量构成分组柱状图，用于展示各个分类下的不同分组
@@ -12,9 +17,33 @@
 :::include(src="./doc/group.vue")
 :::
 
+## 双Y轴柱状图
+
+> 有两个Y轴的柱状图，用于不同的数值类型
+
+:::include(src="./doc/multiY-two.vue")
+:::
+
+## 三Y轴柱状图
+
+> 有三个Y轴的柱状图，用于不同的数值类型
+
+:::include(src="./doc/multiY-three.vue")
+:::
+
 ## 堆叠柱状图
 
 > 配置需要堆叠的度量，下例为将不同分组下的 PV、UV 堆叠
 
 :::include(src="./doc/stack.vue")
+:::
+
+## 全局设置数据标签
+
+:::include(src="./doc/simpleLabel.vue")
+:::
+
+## 为每一项单独设置数据标签
+
+:::include(src="./doc/individualLabel.vue")
 :::
